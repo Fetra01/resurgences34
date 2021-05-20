bb6fd38193523f08a23ab892d739aa37d909cec4
Fetra01 <fetra.andria@outlook.fr>
2021-05-20 16:10:09 +0200

    Barre de navigation responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,30 @@
 import React, { useState } from 'react';
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
 
 const Navigation = () => {
-    const [togglerButton, setTogglerButton] = useState(false);
-
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a href="#" className="navbar-brand">Resurgences 34</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation" onClick={(e) => setTogglerButton(!togglerButton)}><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="">
-                    <ul className="navbar-nav mr-auto mg-2">
-                        <li className="nav-item">
-                            <a href="#" className="nav-link active" aria-current="page">Accueil</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Actions</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Contacts</a>
-                        </li>
-                    </ul>
-                </div>
-                
-                
-            </div>
-        </nav>
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Resurgences 34</NavbarBrand>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem active>
+                        <NavLink>Accueil</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>Nous</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>Notre Travailles</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>Contact</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+
+        </Navbar>
     );
 };
 
